6c7ad7b2b86644d973ed3e6db3451bc669652ae8
Marcus P S <msilva@bbn.com>
2016-10-05 16:45:57 -0400

    Fixed path in test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,33 +5,33 @@ using Base.Test, MathieuFunctions
 @test norm(CharacteristicB(0,k=1:100) - [1:100;].^2) == 0
 
 begin
-    test1 = readcsv("./test/MathieuCharacteristicA-1.csv");
+    test1 = readcsv("./MathieuCharacteristicA-1.csv");
     @test (test1 - reduce(hcat,Vector{Float64}[CharacteristicA(q,k=0:10) for q in [-10:.01:10;]]) |> abs |> maximum) < 7.5e-13
 end
 
 begin
-    test1 = readcsv("./test/MathieuCharacteristicA-2.csv");
+    test1 = readcsv("./MathieuCharacteristicA-2.csv");
     @test (test1 - reduce(hcat,Vector{Float64}[CharacteristicA(q,k=0:3) for q in [30:.01:50;]]) |> abs |> maximum) < 7.6e-13
 end
 
 begin
-    test1 = readcsv("./test/MathieuCharacteristicB-1.csv");
+    test1 = readcsv("./MathieuCharacteristicB-1.csv");
     @test (test1 - reduce(hcat,Vector{Float64}[CharacteristicB(q,k=1:10) for q in [-10:.01:10;]]) |> abs |> maximum) < 7.5e-13
 end
 
 begin
-    test1 = readcsv("./test/MathieuCharacteristicB-2.csv");
+    test1 = readcsv("./MathieuCharacteristicB-2.csv");
     @test (test1 - reduce(hcat,Vector{Float64}[CharacteristicB(q,k=1:3) for q in [30:.01:50;]]) |> abs |> maximum) < 2.8e-11
 end
 
 begin
-    test1 = readcsv("./test/MathieuCharacteristicL-1.csv")[1:100,:]
+    test1 = readcsv("./MathieuCharacteristicL-1.csv")[1:100,:]
     test2 = Float64[Characteristicλ(ν,q,k=1:1)[1] for ν in [0:.01:0.99;], q in [-5:.01:5;]]
     (test1 - test2 |> abs |> maximum, test3 - test4 |> abs |> maximum) < 7.5e-15
 end
 
 begin
-    test1 = readcsv("./test/MathieuCharacteristicL-2.csv")[1:100,:]
+    test1 = readcsv("./MathieuCharacteristicL-2.csv")[1:100,:]
     test2 = Float64[Characteristicλ(ν,q,k=1:1)[1] for ν in [0:.01:0.99;], q in [30:.01:50;]]
     (test1 - test2 |> abs |> maximum, test3 - test4 |> abs |> maximum) < 4.5e-14
 end
